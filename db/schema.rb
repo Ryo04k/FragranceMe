7377a97a509cc86b1ff2c3593e9ae3b435ee20d3
Ryo <ryo.kunimasa0416@gmail.com>
2024-10-29 20:05:06 +0900

    add: shop_imageモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_29_104933) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_29_110339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shop_images", force: :cascade do |t|
+    t.bigint "shop_id", null: false
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_shop_images_on_shop_id"
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string "name", null: false
@@ -43,4 +51,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_29_104933) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "shop_images", "shops"
 end
