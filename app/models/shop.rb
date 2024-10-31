95e6c0fae1923fdaf527c693f41112a6c0dd2ea4
Ryo <ryo.kunimasa0416@gmail.com>
2024-10-31 16:31:24 +0900

    add: 店舗画像を表示

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,4 +4,12 @@ class Shop < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     [ "name" ]
   end
+
+  # shop_imageからimage属性を取得し、GoogleMapsAPIから画像を取得するためのURLを生成
+  def photo_urls
+    shop_images.map do |shop_image|
+      "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{shop_image.image}&key=#{GoogleApi.api_key}"
+    end
+  end
+
 end
---
+++ config/initializers/google_api.rb
@@ -0,0 +1,5 @@
+module GoogleApi
+  def self.api_key
+    ENV['GOOGLE_API_KEY']
+  end
+end
