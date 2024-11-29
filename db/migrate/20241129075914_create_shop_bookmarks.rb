98a13ba1785e99d3f93bac12b829d8122de31b74
Ryo <ryo.kunimasa0416@gmail.com>
2024-11-29 17:00:29 +0900

    add: shop_bookmarkモデルを作成

---
+++ db/migrate/20241129075914_create_shop_bookmarks.rb
@@ -0,0 +1,10 @@
+class CreateShopBookmarks < ActiveRecord::Migration[7.2]
+  def change
+    create_table :shop_bookmarks do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :shop, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
