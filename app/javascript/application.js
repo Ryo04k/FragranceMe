ecc46ddc17f611b9203d62ac475590a3d00da015
Ryo <ryo.kunimasa0416@gmail.com>
2024-12-02 17:10:17 +0900

    add: rails-ujsのインストール

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import Rails from "@rails/ujs"
+Rails.start()
 
 document.addEventListener("turbo:click", (event) => {
     if (event.target.closest('.pagination')) {
