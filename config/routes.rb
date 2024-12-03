8d981989c8e97ff87a1d39380ae4e988f48d0a98
Ryo <ryo.kunimasa0416@gmail.com>
2024-12-03 12:19:16 +0900

    add: ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :bookmarks, only: %i[create destroy]
+  resources :shop_bookmarks, only: %i[create destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
