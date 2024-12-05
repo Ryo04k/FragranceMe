720c7df19f3961777362e527c38b7ae4e9d897b1
Ryo <ryo.kunimasa0416@gmail.com>
2024-12-05 16:14:12 +0900

    add: ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
 
   resources :shops do
+    resources :reviews, only: %i[new create show edit update destroy], shallow: true
     collection do
       get "search"
       get "map"
