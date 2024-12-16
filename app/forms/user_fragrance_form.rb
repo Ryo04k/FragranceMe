ed1b4ace4ec210e37889515bfd3b3c8c7aa33445
Ryo <ryo.kunimasa0416@gmail.com>
2024-12-16 17:54:04 +0900

    add: 質問と回答を追加

--- app/forms/user_fragrance_form.rb
+++ app/forms/user_fragrance_form.rb
@@ -2,33 +2,36 @@ class UserFragranceForm
   include ActiveModel::Model
 
   ANSWER_MAPPING = {
-    "1" => "初心者",
-    "2" => "ある程度詳しい",
-    "3" => "詳しい",
-    "4" => "アクティブ",
-    "5" => "仕事中心で忙しい",
-    "6" => "趣味に時間を使う",
-    "7" => "日常使い",
-    "8" => "特別な日",
-    "9" => "ビジネスシーン",
+    "1"  => "初心者",
+    "2"  => "ある程度詳しい",
+    "3"  => "詳しい",
+    "4"  => "アクティブ",
+    "5"  => "仕事中心で忙しい",
+    "6"  => "趣味に時間を使う",
+    "7"  => "日常使い",
+    "8"  => "特別な日",
+    "9"  => "ビジネスシーン",
     "10" => "リラックスしたい時",
-    "11" => "デートの時",
-    "12" => "カジュアル",
-    "13" => "キレイめ",
-    "14" => "大人っぽい",
-    "15" => "個性的",
-    "16" => "シンプル",
-    "17" => "女性向け",
-    "18" => "男性向け",
-    "19" => "特に無し",
-    "20" => "フラワーブーケ",
-    "21" => "フルーツバスケット",
-    "22" => "スパイシー",
-    "23" => "森の香り",
-    "24" => "フレッシュで爽やか",
-    "25" => "落ち着いて知的",
-    "26" => "華やかで女性らしい"
+    "11" => "カジュアル",
+    "12" => "キレイめ",
+    "13" => "個性的",
+    "14" => "シンプル",
+    "15" => "女性向け",
+    "16" => "男性向け",
+    "17" => "特に無し",
+    "18" => "フラワーブーケ",
+    "19" => "フルーツバスケット",
+    "20" => "スパイシー",
+    "21" => "森の香り",
+    "22" => "春",
+    "23" => "夏",
+    "24" => "秋",
+    "25" => "冬",
+    "26" => "フレッシュで爽やか",
+    "27" => "落ち着いて知的",
+    "28" => "華やかで女性らしい"
   }
+
 
   attr_accessor :diagnosis_id, :user_answers
 
@@ -50,12 +53,10 @@ class UserFragranceForm
       "特別な日" => { floral: 1, citrus: 0, oriental: 1, spicy: 0, herbal: 0, woody: 0 },
       "ビジネスシーン" => { floral: 0, citrus: 0, oriental: 1, spicy: 0, herbal: 0, woody: 1 },
       "リラックスしたい時" => { floral: 1, citrus: 1, oriental: 0, spicy: 0, herbal: 1, woody: 0 },
-      "デートの時" => { floral: 1, citrus: 0, oriental: 1, spicy: 0, herbal: 0, woody: 0 }
     },
     fashion: {
       "カジュアル" => { floral: 1, citrus: 2, oriental: 0, spicy: 0, herbal: 1, woody: 0 },
       "キレイめ" => { floral: 2, citrus: 1, oriental: 1, spicy: 0, herbal: 0, woody: 1 },
-      "大人っぽい" => { floral: 1, citrus: 0, oriental: 2, spicy: 2, herbal: 0, woody: 2 },
       "個性的" => { floral: 1, citrus: 1, oriental: 1, spicy: 2, herbal: 1, woody: 1 },
       "シンプル" => { floral: 0, citrus: 1, oriental: 0, spicy: 0, herbal: 2, woody: 2 }
     },
@@ -69,6 +70,12 @@ class UserFragranceForm
       "フルーツバスケット" => { floral: 0, citrus: 2, oriental: 0, spicy: 0, herbal: 0 },
       "スパイシー" => { floral: 0, citrus: 0, oriental: 2, spicy: 2, herbal: 0 },
       "森の香り" => { floral: 0, citrus: 0, oriental: 1, spicy: 1, herbal: 2 }
+    },
+    season: {
+    "春" => { floral: 2, citrus: 1, oriental: 0, spicy: 0, herbal: 1, woody: 0 },
+    "夏" => { floral: 1, citrus: 2, oriental: 0, spicy: 0, herbal: 1, woody: 0 },
+    "秋" => { floral: 1, citrus: 1, oriental: 2, spicy: 1, herbal: 0, woody: 1 },
+    "冬" => { floral: 0, citrus: 0, oriental: 2, spicy: 2, herbal: 0, woody: 2 }
     },
     impression: {
       "フレッシュで爽やか" => { floral: 1, citrus: 2, oriental: 0, spicy: 0, herbal: 2, woody: 0 },
