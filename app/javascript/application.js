a6285efceca90801d65c5ef4beb8b9d8b13afd6e
Ryo <ryo.kunimasa0416@gmail.com>
2024-12-30 14:18:57 +0900

    fix: スムーススクロールを解除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,7 +8,6 @@ document.addEventListener("turbo:click", (event) => {
     if (event.target.closest('.pagination')) {
         window.scroll({
             top: 0,
-            behavior: 'smooth'
         });
     }
 });
