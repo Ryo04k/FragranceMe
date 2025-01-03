e242be420f38b2d2a2c090592585b951fa4c1597
Ryo <ryo.kunimasa0416@gmail.com>
2025-01-03 10:28:02 +0900

    update: 取得画像のサイズを変更

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -25,10 +25,9 @@ class Shop < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: ->(obj) { obj.address.present? && obj.address_changed? }
 
-  # shop_imageからimage属性を取得し、GoogleMapsAPIから画像を取得するためのURLを生成
   def photo_urls
     shop_images.map do |shop_image|
-      "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1200&photoreference=#{shop_image.image}&key=#{GoogleApi.api_key}"
+      "https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photoreference=#{shop_image.image}&key=#{GoogleApi.api_key}"
     end
   end
 end
