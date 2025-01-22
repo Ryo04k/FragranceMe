e4ca05da487a58a549fcb4ee4c944386e2f095aa
Ryo <ryo.kunimasa0416@gmail.com>
2025-01-22 21:27:11 +0900

    add：meta情報の作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,30 @@ module ApplicationHelper
     else "bg-gray-500"
     end
   end
+
+  def default_meta_tags
+    {
+      site: 'FragranceMe',
+      title: 'フレグランスショップ検索サービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'FragranceMeは、気になるフレグランスショップを簡単に見つけることができるサービスです。香り探しをもっと楽しく、快適に！',
+      keywords: 'Fragrance,フレグランス,香り',
+      canonical: "https://www.fragrance-me.com/",
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: "https://www.fragrance-me.com/",
+        image: image_url('ogp_01.jpg'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        image: image_url('ogp_01.jpg')
+      }
+    }
+  end
 end
