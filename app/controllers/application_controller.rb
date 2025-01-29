536c501f3aebd125e434e16160a88c2d4df25ecf
Ryo <ryo.kunimasa0416@gmail.com>
2025-01-29 16:23:00 +0900

    remove：ベーシック認証を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   # 診断結果のセッションデータ
@@ -7,15 +6,8 @@ class ApplicationController < ActionController::Base
 
   private
 
-
   # ログイン後のリダイレクト先を設定
   def after_sign_in_path_for(resource_or_scope)
     shops_path
-  end
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
   end
 end
